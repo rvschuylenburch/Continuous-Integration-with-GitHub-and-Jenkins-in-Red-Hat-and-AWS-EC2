da01a17dc03dedf4b3da6994553bacd5b197da14
Turreta.com <Turreta@users.noreply.github.com>
2017-09-24 21:28:41 +0800

    Update ComTurretaGithubjenkinsCiDemoApplication.java

--- src/main/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplication.java
+++ src/main/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplication.java
@@ -8,6 +8,6 @@ public class ComTurretaGithubjenkinsCiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComTurretaGithubjenkinsCiDemoApplication.class, args);
-		System.out.println("Test Jenkin build triggers");
+		System.out.println("Test Jenkins build triggers");
 	}
 }
