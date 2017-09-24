3c32d9138ed7d4875e6b7086cb2f6bd88f9d0d0f
Karl San Gabriel <karl.sangabriel@gmail.com>
2017-09-24 21:50:52 +0800

    Test Jenkins Build Trigger
    Test Jenkins Build Trigger

--- src/main/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplication.java
+++ src/main/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplication.java
@@ -9,5 +9,6 @@ public class ComTurretaGithubjenkinsCiDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComTurretaGithubjenkinsCiDemoApplication.class, args);
 		System.out.println("Test Jenkins build triggers");
+		System.out.println("Pushed from GitHub Desktop!");
 	}
 }
