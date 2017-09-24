6312140dffa78fe9206ae2b1de1f7d116bfea3ef
Turreta.com <Turreta@users.noreply.github.com>
2017-09-24 19:28:31 +0800

    Add files via upload

---
+++ src/main/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.turreta.githubjenkins.ci.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTurretaGithubjenkinsCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComTurretaGithubjenkinsCiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/turreta/githubjenkins/ci/demo/ComTurretaGithubjenkinsCiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turreta.githubjenkins.ci.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComTurretaGithubjenkinsCiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
